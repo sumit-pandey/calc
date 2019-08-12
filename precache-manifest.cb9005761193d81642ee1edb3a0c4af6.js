8bfabe66941f561876780f94f508c8c826d16a97
Kurtika <kurtika@Plotlabss-MacBook-Air.local>
2019-08-13 02:55:41 +0530

    Updates

---
+++ precache-manifest.cb9005761193d81642ee1edb3a0c4af6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977cdcb9a9aa4ee931f3f6e1590ae71f",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "53faa7f134d2c6f46c7a",
+    "url": "/calc/static/css/main.1dc6844e.chunk.css"
+  },
+  {
+    "revision": "c6e6d131bbf299d2dcce",
+    "url": "/calc/static/js/2.2321b587.chunk.js"
+  },
+  {
+    "revision": "53faa7f134d2c6f46c7a",
+    "url": "/calc/static/js/main.ce0759d5.chunk.js"
+  },
+  {
+    "revision": "24a6bdf9cac3708c0fa4",
+    "url": "/calc/static/js/runtime~main.9bea966d.js"
+  }
+]);
